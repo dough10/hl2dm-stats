17becaca5b139a67b722d384639fa9c0d1cc1bf6
dough10 <doughten.jimi@gmail.com>
2020-10-21 12:01:58 -0500

    no + '

--- api.js
+++ api.js
@@ -370,7 +370,7 @@ function scanLine(line) {
     }
     var said = '';
     for (var i = (isChat + 1); i < word.length; i++) {
-      said = said + word[i] + ' ';
+      said = `${said}${word[i]} `;
     }
     said.replace('"', '');
     said.replace('"', '');
@@ -434,10 +434,18 @@ function getFilesizeInBytes(filename) {
 }
 
 function bytesToSize(bytes) {
-   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
-   if (bytes == 0) return '0 Byte';
+   var sizes = [
+     'Bytes',
+     'KB',
+     'MB',
+     'GB',
+     'TB'
+   ];
+   if (bytes == 0) {
+     return '0 Byte';
+   }
    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-   return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+   return `${Math.round(bytes / Math.pow(1024, i), 2)} ${sizes[i]}`;
 }
 
 var updated = false;
