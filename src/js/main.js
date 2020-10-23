427dbd829963e7da354e02a645eca35d6a8550f0
dough10 <doughten.jimi@gmail.com>
2020-10-22 19:10:54 -0500

    no toast for you

--- src/js/main.js
+++ src/js/main.js
@@ -305,7 +305,7 @@ function parseServerStatus(status) {
         }
         if (!playersOnline.includes(playerName)) {
           playersOnline.push(playerName);
-          new animations.Toast(`${playerName} has joined the game`, 2);
+          // new animations.Toast(`${playerName} has joined the game`, 2);
         }
         console.log(playersOnline, 'before loop')
         for (var ndx = 0; ndx < playersOnline.length; ndx++) {
