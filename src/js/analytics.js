582838ef31e3a9f4598b52d96d21d8a334930838
dough10 <doughten.jimi@gmail.com>
2020-10-25 14:13:56 -0500

    /shrug

--- src/js/analytics.js
+++ src/js/analytics.js
@@ -1,3 +1,8 @@
+var _mtm = window._mtm = window._mtm || [];
+_mtm.push({'mtm.startTime': (new Date().getTime()), 'event': 'mtm.Start'});
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+g.type='text/javascript'; g.async=true; g.src='https://analytics.dough10.me/js/container_zeRUcLbM.js'; s.parentNode.insertBefore(g,s);
+
 var _paq = window._paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['trackPageView']);
