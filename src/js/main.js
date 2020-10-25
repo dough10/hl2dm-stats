e409f37bba00ce62f59befccde964ef59532f310
dough10 <doughten.jimi@gmail.com>
2020-10-25 14:51:26 -0500

    both & more

--- src/js/main.js
+++ src/js/main.js
@@ -189,34 +189,33 @@ function showApp() {
     animations.animateElement(qs('#load'), 'translateY(-102%)', 350).then(_ => {
       if (new Date().getDate() === 1) {
         animations.animateElement(qs('#alert'), 'translateY(0)', 200, 1, 0);
-      } else {
-        switch (numPlayersOnline) {
-          case 0:
-            new animations.Toast(`${numPlayersOnline} players online.`, 2);
-            break;
-          case 1:
-            new animations.Toast(`${numPlayersOnline} players online. He needs someone to kill`, 2);
-            break;
-          case 2:
-            new animations.Toast(`${numPlayersOnline} players online. 1v1 in progress`, 2);
-            break;
-          case 3:
-            new animations.Toast(`${numPlayersOnline} players online. Deathmatch had begun`, 2);
-            break;
-          case 4:
-            new animations.Toast(`${numPlayersOnline} players online. Shits poppin off`, 2);
-            break;
-          case 5:
-            new animations.Toast(`${numPlayersOnline} players online. Room for one more`, 2);
-            break;
-          case 6:
-            new animations.Toast(`${numPlayersOnline} players online. Server full`, 2);
-            break;
-        }
-        loaded = true;
       }
+      switch (numPlayersOnline) {
+        case 0:
+          new animations.Toast(`${numPlayersOnline} players online.`, 2);
+          break;
+        case 1:
+          new animations.Toast(`${numPlayersOnline} players online. He needs someone to kill`, 2);
+          break;
+        case 2:
+          new animations.Toast(`${numPlayersOnline} players online. 1v1 in progress`, 2);
+          break;
+        case 3:
+          new animations.Toast(`${numPlayersOnline} players online. Deathmatch had begun`, 2);
+          break;
+        case 4:
+          new animations.Toast(`${numPlayersOnline} players online. Shits poppin off`, 2);
+          break;
+        case 5:
+          new animations.Toast(`${numPlayersOnline} players online. Room for one more`, 2);
+          break;
+        case 6:
+          new animations.Toast(`${numPlayersOnline} players online. Server full`, 2);
+          break;
+      }
+      loaded = true;
     });
-  }, 1000);
+  }, 1200);
 }
 
 function formatNumber(num) {
@@ -227,6 +226,9 @@ function parseTopData(top) {
   const killsIcon = "M7,5H23V9H22V10H16A1,1 0 0,0 15,11V12A2,2 0 0,1 13,14H9.62C9.24,14 8.89,14.22 8.72,14.56L6.27,19.45C6.1,19.79 5.76,20 5.38,20H2C2,20 -1,20 3,14C3,14 6,10 2,10V5H3L3.5,4H6.5L7,5M14,12V11A1,1 0 0,0 13,10H12C12,10 11,11 12,12A2,2 0 0,1 10,10A1,1 0 0,0 9,11V12A1,1 0 0,0 10,13H13A1,1 0 0,0 14,12Z";
   const deathsIcon = "M12,2A9,9 0 0,0 3,11C3,14.03 4.53,16.82 7,18.47V22H9V19H11V22H13V19H15V22H17V18.46C19.47,16.81 21,14 21,11A9,9 0 0,0 12,2M8,11A2,2 0 0,1 10,13A2,2 0 0,1 8,15A2,2 0 0,1 6,13A2,2 0 0,1 8,11M16,11A2,2 0 0,1 18,13A2,2 0 0,1 16,15A2,2 0 0,1 14,13A2,2 0 0,1 16,11M12,14L13.5,17H10.5L12,14Z";
   const kdrIcon =   "M3 18.34C3 18.34 4 7.09 7 3L12 4L11 7.09H9V14.25H10C12 11.18 16.14 10.06 18.64 11.18C21.94 12.71 21.64 17.32 18.64 19.36C16.24 21 9 22.43 3 18.34Z";
+  if (!top[0].length) {
+    animations.animateElement(qs('#alert'), 'translateY(0)', 200, 1, 0);
+  }
   for (let i = 0; i < top[0].length; i++) {
     const player = top[0][i];
     const wrapper = createWrapper();
