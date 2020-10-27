f668e77485ac1961fa8e0c5a36a48a6056e4de77
dough10 <doughten.jimi@gmail.com>
2020-10-27 16:36:59 -0500

    test cleanup

---
+++ cleanuptest.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const config = require(`${__dirname}/config.json`);
+const logFolder = path.join(config.gameServerDir, 'logs');
+
+
+function cleanUp() {
+  console.count('cleanup-function')
+  console.log(`${new Date()} - Running file clean up`);
+  var numFiles = 0;
+  fs.readdir(logFolder, (err, files) => {
+    numFiles = numFiles + files.length;
+    files.forEach(console.log);
+    fs.readdir(config.gameServerDir, (err, files) => {
+      files.forEach(file => {
+        numFiles = numFiles + files.length;
+        if (path.extname(file) === '.dem') {
+          console.log(file);
+        }
+      });
+    });
+  });
+  console.log(`${new Date()} - Clean up complete. Removed ${numFiles} files`);
+;}
