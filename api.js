9235ab835aac949a886950b4a94b8a8dbe34bd4f
dough10 <doughten.jimi@gmail.com>
2020-10-27 15:44:34 -0500

    dsassasa

--- api.js
+++ api.js
@@ -466,11 +466,8 @@ function scanLine(line) {
     console.log(name, id3);
     for (var i = 1; i < stats.length; i++) {
       var items = stats[i].split(' ');
-      for (var o = 1; o < items.length; o++) {
+      for (var o = 0; o < items.length; o++) {
         items[o] = items[o].replace('"', '').replace('"', '').replace(')', '');
-        if (o > 1) {
-          items[o] = Number(items[o]);
-        }
       }
       console.log(items)
     }
