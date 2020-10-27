7f14cc0a83a48807eaf02ec30062258885b504c3
dough10 <doughten.jimi@gmail.com>
2020-10-27 18:01:20 -0500

    connected?

--- api.js
+++ api.js
@@ -629,6 +629,9 @@ app.get('/demos', (reg,res) => {
 });
 
 app.ws('/', (ws, req) => {
+  ws.on('connection', _ => {
+    console.log('connected?')
+  })
   ws.on('message', (msg) => {
     console.log(msg);
   });
