a55ed6ec21b77ee05837c873905eda60ba8e811a
dough10 <doughten.jimi@gmail.com>
2020-10-29 05:13:25 -0500

    repeat it for now

--- src/js/modules/toast.js
+++ src/js/modules/toast.js
@@ -1,5 +1,24 @@
 export {Toast};
 
+function whichTransitionEvent() {
+  let t;
+  const el = document.createElement('fakeelement');
+  const transitions = {
+    'WebkitTransition' :'webkitTransitionEnd',
+    'MozTransition'    :'transitionend',
+    'MSTransition'     :'msTransitionEnd',
+    'OTransition'      :'oTransitionEnd',
+    'transition'       :'transitionEnd'
+  };
+  for (t in transitions) {
+    if (el.style[t] !== undefined ) {
+      return transitions[t];
+    }
+  }
+}
+// the browser transition event name
+const transitionEvent = whichTransitionEvent();
+
 /**
  * display a toast message
  *
