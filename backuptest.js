7e3a65ec67f219da6e9071fa98d1b0a898c27154
dough10 <doughten.jimi@gmail.com>
2020-10-30 16:48:31 -0500

    dasas

--- backuptest.js
+++ backuptest.js
@@ -2,7 +2,7 @@ const path = require('path');
 const fs = require('fs');
 const config = require(`${__dirname}/config.json`);
 const logFolder = path.join(config.gameServerDir, 'logs');
-var JSZip = require("jszip");
+const child_process = require("child_process");
 var top = {data: 'is here'};
 
 
@@ -10,33 +10,26 @@ function cleanUp() {
   console.count('cleanup-function')
   var now = new Date();
   var lastMonth = now.setMonth(now.getMonth() - 1);
-  var folder = `${__dirname}/oldTop`;
   var zip = new JSZip();
+  var folder = `${__dirname}/oldTop`;
   if (!fs.existsSync(folder)){
     fs.mkdirSync(folder);
   }
-  var filename = `${__dirname}/oldTop/${lastMonth}.json`;
-  fs.writeFile(filename, JSON.stringify(top), e => {
-    if (e) return console.log(`${new Date()} - Error saving ${__dirname}/oldTop/${lastMonth}.json`, err);
-    if (!fs.existsSync(filename)){
-      return console.log(`${new Date()} - Error saving ${__dirname}/oldTop/${lastMonth}.json`);
-    }
-    console.log(`${new Date()} - top player data saved as ${__dirname}/oldTop/${lastMonth}.json`);
+  copyLogsFiles(lastMonth).then(saveOldTop).then(_ => {
     var numFiles = 0;
     fs.readdir(logFolder, (err, files) => {
       console.log(`${new Date()} - Running log file clean up`);
       numFiles = numFiles + files.length;
       var howMany = files.length;
       files.forEach(file => {
-        copyFiles(logFolder, file, lastMonth);
-        // delete path.join(logFolder, file);
+        console.log(path.join(logFolder, file));
       });
       fs.readdir(config.gameServerDir, (err, files) => {
         console.log(`${new Date()} - Running demo file clean up`);
         files.forEach(file => {
           numFiles = numFiles + files.length;
           if (path.extname(file) === '.dem') {
-            // copyFiles(config.gameServerDir, file, lastMonth);
+            console.log(path.join(config.gameServerDir, file));
             howMany--;
             if (howMany <= 0) {
               console.log(`${new Date()} - Clean up complete. ${numFiles} files processed and backed up to ${__dirname}/oldLogs/${lastMonth}`);
@@ -46,18 +39,42 @@ function cleanUp() {
         });
       });
     });
+  }).catch(_ => {
+    console.log(`${new Date()} - Error saving ${__dirname}/oldTop/${lastMonth}.json`);
   });
 }
 
-function copyFiles(root, filename, lastMonth) {
-  var folder = './oldLogs';
-  if (!fs.existsSync(folder)){
-    fs.mkdirSync(folder);
-  }
-  if (!fs.existsSync(`${__dirname}/oldLogs/${lastMonth}`)){
-    fs.mkdirSync(`${__dirname}/oldLogs/${lastMonth}`);
-  }
-  // fs.createReadStream(path.join(root, filename)).pipe(fs.createWriteStream(`${__dirname}/oldLogs/${lastMonth}/${filename}`));
+function saveOldTop() {
+  return new Promise((resolve, reject) => {
+    var filename = `${__dirname}/oldTop/${lastMonth}.json`;
+    fs.writeFile(filename, JSON.stringify(top), e => {
+      if (e) {
+        reject();
+      }
+      if (!fs.existsSync(filename)){
+        reject();
+      }
+      console.log(`${new Date()} - top player data saved as ${__dirname}/oldTop/${lastMonth}.json`);
+      resolve();
+    });
+  });
+}
+
+
+function copyLogsFiles(lastMonth) {
+  return new Promise((resolve, reject) => {
+    var folder = './oldLogs';
+    if (!fs.existsSync(folder)){
+      fs.mkdirSync(folder);
+    }
+    if (!fs.existsSync(`${__dirname}/oldLogs/${lastMonth}`)){
+      fs.mkdirSync(`${__dirname}/oldLogs/${lastMonth}`);
+    }
+    child_process.execSync(`zip -r ${__dirname}/oldLogs/${lastMonth}.zip ${lastMonth}*`, {
+      cwd: '/appdata/hl2dm/hl2mp/logs'
+    });
+    resolve();
+  });
 }
 
 cleanUp();
