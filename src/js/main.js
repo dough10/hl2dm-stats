80ffd6807ae09c65f364f498e39d4008567f1ce1
dough10 <doughten.jimi@gmail.com>
2020-10-31 11:56:33 -0500

    sp

--- src/js/main.js
+++ src/js/main.js
@@ -207,7 +207,7 @@ function displayPlayerOnline(playersOnline) {
       var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
       var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
       var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-      qs('#soon-text').textContent = `in ${hours} hours ${minutes }minutes ${seconds} seconds`;
+      qs('#soon-text').textContent = `in ${hours} hours ${minutes} minutes ${seconds} seconds`;
       if (distance < 0) {
         clearInterval(x);
       }
