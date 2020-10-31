25b80393a74697a8f467462d6d77cb2790a93f6d
dough10 <doughten.jimi@gmail.com>
2020-10-30 20:02:18 -0500

    aaaaaaaa

--- api.js
+++ api.js
@@ -282,8 +282,8 @@ function lineIsConsole(line) {
   for (var i = 0; i < line.length; i++) {
     if (line[i] === 'rcon') {
       if (line[i + 1] === 'from') {
+        console.log(line[i + 2].slice(0, -1).replace('"', '').replace('"', ''))
         if (line[i + 2].slice(0, -1).replace('"', '').replace('"', '') === ipstring) {
-          console.log(line[i + 2])
           return i;
         }
       }
