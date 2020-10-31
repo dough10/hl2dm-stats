ac591ea4ac227dda299b3a0406f288b9f6135e80
dough10 <doughten.jimi@gmail.com>
2020-10-31 08:09:48 -0500

    dsfa

--- src/js/main.js
+++ src/js/main.js
@@ -194,8 +194,11 @@ function showApp() {
 
 function displayPlayerOnline(playersOnline) {
   var now = new Date();
+  now.setHours(5);
+  now.setMinuites(0);
+  now.setSeconds(0);
   now.setMonth(now.getMonth() + 1, 1);
-  qs('#soon-text').textContent = `${new Date(now).toDateString()} @ 5am CST`;
+  qs('#soon-text').textContent = `${new Date(now).totoUTCString()}`;
   if (new Date().getDate() >= 28) {
     animations.animateElement(qs('#soon'), 'translateY(0)', 800, 1, 0);
   }
