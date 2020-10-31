e56b21030be3f280287364ea8108fabfd2fb25f0
dough10 <doughten.jimi@gmail.com>
2020-10-30 20:06:52 -0500

    ignore rcon lines

--- api.js
+++ api.js
@@ -301,7 +301,7 @@ function scanLine(line) {
   var isStats = lineIsStats(word);
   var isConsole = lineIsConsole(word);
   if (isConsole) {
-    console.log(line);
+     return;
   } else if (isChat) {
     const lineTime = new Date(`${word[3].slice(0, -1)} ${word[1]}`).getTime();
     const nameString = buildKillerNameString(word, isChat);
