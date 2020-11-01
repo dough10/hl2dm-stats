ed38516f0adcd7ec60922aae1e719042bc0b129e
dough10 <doughten.jimi@gmail.com>
2020-11-01 07:19:16 -0600

    as

--- src/js/main.js
+++ src/js/main.js
@@ -200,7 +200,7 @@ function displayPlayerOnline(playersOnline) {
   resetTime.setMinutes(0);
   resetTime.setSeconds(0);
   resetTime.setMonth(resetTime.getMonth() + 1, 1);
-  console.log(loadtime.getDate() <= 2, loadtime.getDate() === lastDay, loadtime.getDate() > lastDay - 3)
+  console.log(lastDay, loadtime.getDate() > lastDay - 3)
   if (loadtime.getDate() <= 2) {
     animations.animateElement(qs('#reset'), 'translateY(0)', 800, 1, 0);
   } else if (loadtime.getDate() === lastDay) {
