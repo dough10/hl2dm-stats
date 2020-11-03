6ce3146f39fdf6c011f204a3e1ad6a0baf7f2f63
dough10 <doughten.jimi@gmail.com>
2020-11-02 18:11:20 -0600

    asdasssssss

--- src/js/main.js
+++ src/js/main.js
@@ -487,7 +487,7 @@ function fetchOldMonths(month) {
         for (var i = 0; i < months.length; i++) {
           months[i] = months[i].replace('.json', '');
         }
-        console.log(new Date(months[0]).getMonth());
+        console.log(new Date(Number(months[0])).getMonth());
         // fetchOldMonths();
       });
     });
