6391f936047f20edf85434cbfc6c637ad5690c37
dough10 <doughten.jimi@gmail.com>
2020-11-02 18:15:52 -0600

    sdasda

--- api.js
+++ api.js
@@ -675,6 +675,7 @@ function getOldStatsList(res, month) {
         console.log(files[i])
         var date = path.basename(files[i]);
         var fileMonth = new Date(Number(date)).getMonth();
+        console.log(fileMonth === month)
         if (fileMonth === month) {
           var data = require(`${__dirname}/old-top/${files[i]}`);
           res.send(data);
