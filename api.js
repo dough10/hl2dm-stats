61b613181acca377c25ad86226435edb43c8a362
dough10 <doughten.jimi@gmail.com>
2020-11-08 03:22:27 -0600

    sdawe21

--- api.js
+++ api.js
@@ -542,7 +542,7 @@ function sortWeapons(user) {
   var sortArr = [];
   for (var weapon in user) {
     if (isWeapon(weapon)) {
-      console.log(weaponStats[user.id][weapon]);
+      console.log(user, weaponStats[user.id][weapon]);
       sortArr.push([weapon, user[weapon]]);
       delete user[weapon];
     }
