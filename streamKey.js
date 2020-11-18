08330f0bc96817e732e74b5fb44ad69b5ba1bc06
dough10 <doughten.jimi@gmail.com>
2020-11-18 16:16:09 -0600

    too strong

--- streamKey.js
+++ streamKey.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 var args = process.argv.slice(2);
-bcrypt.hash(args[0], 100, function(err, hash) {
+bcrypt.hash(args[0], 10, function(err, hash) {
   if (err) {
     return console.error(err);
   }
