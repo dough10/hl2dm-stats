50ee87f31cf100b56372eb54578e6c4300a7559a
dough10 <doughten.jimi@gmail.com>
2020-11-20 15:32:39 -0600

    1231232

--- src/js/main.js
+++ src/js/main.js
@@ -516,7 +516,7 @@ function parseTopData(top, page, cb) {
     const icon = document.createElement('div');
     const text = document.createElement('div');
     tooltip.classList.add('tooltiptext');
-    tooltip.appendChild(tooltipHTML(fav[0], Math.round((fav[1] / player.kills, fav[2][0], fav[2][1], fav[2][2]));
+    tooltip.appendChild(tooltipHTML(fav[0], Math.round((fav[1] / player.kills) * 100), fav[2][0], fav[2][1], fav[2][2]));
     // tooltip.textContent = `${fav[0]}: ${Math.round((fav[1] / player.kills) * 100)}% of all kills`;
     // if (fav[2] && fav[2][0] && fav[2][1] && fav[2][2]) {
     //   tooltip.textContent = `${tooltip.textContent}, ${fav[2][0]} fired shots, ${fav[2][1]}% hit, ${fav[2][2]}% headshots`;
