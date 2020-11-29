aee4151f2043efa7ae1dc40aef74e7790f97e3f4
dough10 <doughten.jimi@gmail.com>
2020-11-29 13:07:33 -0600

    32123132

--- api.js
+++ api.js
@@ -17,9 +17,9 @@ const colors = require('colors');                         // colorize text
 const config = require(`${__dirname}/config.json`);       // config file location
 const logFolder = path.join(config.gameServerDir, 'logs');// game server log location
 const useragent = require('express-useragent');           // user browser data
-// const clear = require('clear');                           // clear screen
+const clear = require('clear');                           // clear screen
 
-// clear();
+clear();
 console.log(figlet.textSync('dough10/hl2dm-stats', { horizontalLayout: 'default' }));
 
 print('Configure Express');
