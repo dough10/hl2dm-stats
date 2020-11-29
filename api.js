4c9f0ae593a1f08451357660030093ab07d471b3
dough10 <doughten.jimi@gmail.com>
2020-11-29 11:09:27 -0600

    321321

--- api.js
+++ api.js
@@ -16,7 +16,7 @@ app.disable('x-powered-by');
 var expressWs = require('express-ws')(app);
 const io = require('@pm2/io');
 const colors = require('colors');
-
+const term = require( 'terminal-kit' ).terminal;
 const config = require(`${__dirname}/config.json`);
 const logFolder = path.join(config.gameServerDir, 'logs');
 
@@ -64,7 +64,17 @@ function ascii(message) {
  */
 function print(message) {
   var now = new Date().toLocaleString();
-  console.log(`${now.yellow} - ${message}`);
+  term.table( [
+		[ascii('dough10/hl2dm-stats')] ,
+		[`${now.yellow} - ${message}`]
+	] , {
+		hasBorder: true ,
+		contentHasMarkup: true ,
+		width: 60 ,
+		fit: true   // Activate all expand/shrink + wordWrap
+	}
+) ;
+  // console.log(`${now.yellow} - ${message}`);
 }
 
 /**
