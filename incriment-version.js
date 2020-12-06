e478b7c2e2a4854ea006defb390a03a87fe44ee8
dough10 <doughten.jimi@gmail.com>
2020-12-06 11:33:49 -0600

    1607276029, V:

--- incriment-version.js
+++ incriment-version.js
@@ -1,7 +1,6 @@
 const fs = require('fs');                                 // work with the file system
 var package = require('./package.json');
-var inc = require('version-incrementer').increment;
-var newVersion = inc(package.version);
+var newVersion = require('version-incrementer').increment(package.version);
 var updated = JSON.stringify({...package, version: newVersion}, null, 2);
 
 fs.writeFile('package.json', updated, e => {
