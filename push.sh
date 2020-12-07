91ee808304b869bb4f41a904b872991cc7fcbb53
dough10 <doughten.jimi@gmail.com>
2020-12-06 19:32:03 -0600

    1607304723, V: 1.0.3

--- push.sh
+++ push.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-if [ "$1" == "+" ]
-  then
-    v=$(node incriment-version.js)
-  else
-    v=$(node -p "require('./package.json').version")
-fi
+# if [ "$1" == "+" ]
+#   then
+#     v=$(node incriment-version.js)
+#   else
+#     v=$(node -p "require('./package.json').version")
+# fi
 d=$(date +%s)
-echo "$d, V:$v"
+echo "$d, V: $1"
 git add .
-git commit -m "$d, V:$v"
+git commit -m "$d, V: $1"
 git push
