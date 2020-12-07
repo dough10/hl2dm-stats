3fe99b8736c97119b0b469e57ef0c234bd25e222
dough10 <doughten.jimi@gmail.com>
2020-12-06 19:22:28 -0600

    1607304148, V:

--- push.sh
+++ push.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-ve=$(node -p "require('./package.json').version")
+v=$(node -p "require('./package.json').version");
 # if [ "$1" == "+" ]
 #   then
-#     ve=$(node incriment-version.js)
+#     v=$(node incriment-version.js)
 # fi
-d=$(date +%s)
+d=$(date +%s);
 git add .
-git commit -m "$d, V: $ve"
+git commit -m "$d, V: $v"
 git push
