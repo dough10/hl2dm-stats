334fd1048e98f72371bdc9e1e47da2ae2c1fe383
dough10 <doughten.jimi@gmail.com>
2020-12-06 23:19:59 -0600

    1607318398, V:

--- start.sh
+++ start.sh
@@ -10,8 +10,12 @@ echo '\e[33m'
 echo 'https://github.com/dough10/hl2dm-stats'
 echo '\e[39mStarting app...'
 sudo mount -a
-sudo chown crumb /appdata && sudo chown crumb /appdata/hl2dm && sudo chown crumb /appdata/hl2dm/hl2mp && sudo chown crumb /appdata/hl2dm/hl2mp/logs && sudo chmod -R 777 /appdata/hl2dm/hl2mp && sudo chmod 644 /var/log/nginx/access.log
-
+sudo chown crumb /appdata
+sudo chown crumb /appdata/hl2dm
+sudo chown crumb /appdata/hl2dm/hl2mp
+sudo chown crumb /appdata/hl2dm/hl2mp/logs
+sudo chmod -R 777 /appdata/hl2dm/hl2mp
+sudo chmod 644 /var/log/nginx/access.log
 pm2 start api.js --watch
 
 echo "resume monitor"
