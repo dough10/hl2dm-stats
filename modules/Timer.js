56f293d48e72f545375295b3e6185c95a63248cf
dough10 <doughten.jimi@gmail.com>
2020-12-07 18:48:59 -0600

    1607388539, V: 1.0.5

--- modules/Timer.js
+++ modules/Timer.js
@@ -3,19 +3,10 @@
  * A class for timing duration of things
  */
 class Timer {
-  constructor(label) {
-    this.hasTime = 'time' in console;
-    this.label = label;
-    if (this.hasTime) {
-      console.time(this.label);
-      return;
-    }
+  constructor() {
     this.startTime = new Date().getTime();
   }
   end() {
-    if (this.hasTime) {
-      return console.timeEnd(this.label);
-    }
     var ms = new Date().getTime() - this.startTime;
     var seconds = ms / 1000;
     var hours = parseInt( seconds / 3600 );
@@ -29,9 +20,6 @@ class Timer {
     ];
   }
   endString() {
-    if (this.hasTime) {
-      return console.timeEnd(this.label);
-    }
     var endTime = this.end();
     return `${endTime[0]} hours ${endTime[1]} minutes ${endTime[2]} seconds`.cyan;
   }
