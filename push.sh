c846fa3d06cadf8119163f351f1e7fe6951b0a70
dough10 <doughten.jimi@gmail.com>
2021-02-06 20:22:43 -0600

    U: fix push order misconfiguration, V: 1.1.3

--- push.sh
+++ push.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-echo "$1, V: $v";
 if [ "$2" == "+" ]
   then
     v=$(node modules/incriment-version.js);
   else
     v=$(node -p "require('./package.json').version");
 fi
+echo "$1, V: $v";
 node make-docs
 git add .;
 git commit -m "$1, V: $v";
