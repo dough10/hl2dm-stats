5db1c384a746450af00885516663a2edbf585c83
dough10 <doughten.jimi@gmail.com>
2021-04-25 01:23:44 +0000

    U: backup 4th time?, V: 1.1.5

--- backup.sh
+++ backup.sh
@@ -10,20 +10,21 @@ echo '\e[33m'
 echo 'https://github.com/dough10/hl2dm-stats'
 echo '\e[39mBackup Running...'
 
+SMB=//192.168.86.2/Main
 STORAGE=/mnt/nas
 SERVER_LOC=/hoedown/hl2mp
 STATS_SERVER_LOC=/home/crumb/hl2dm-stats
 BACKUP_LOC=$STORAGE/DM-backup
 MONTH=$(date +%b)
 
-echo "mounting NAS"
-mountpoint -q $STORAGE && echo "mounted" || sudo mount -t cifs -o username=$1,password=$2 //192.168.86.2/Main $STORAGE
+echo "Mounting "$STORAGE
+mountpoint -q $STORAGE && echo "mounted" || sudo mount -t cifs -o username=$1,password=$2 $SMB $STORAGE
 
-echo "creating "$MONTH" folders"
+echo "Creating "$MONTH" folders"
 sudo mkdir -p -m 777 $BACKUP_LOC/logs/$MONTH
 sudo mkdir -p -m 777 $BACKUP_LOC/demos/$MONTH
 
-echo "copying files"
+echo "Copying files"
 sudo cp -r -u -v -p $SERVER_LOC/logs/* $BACKUP_LOC/logs/$MONTH
 sudo cp -r -u -v -p $SERVER_LOC/*.dem $BACKUP_LOC/demos/$MONTH 
 sudo cp -r -u -v -p $STATS_SERVER_LOC/old-top/* $BACKUP_LOC/old-top
