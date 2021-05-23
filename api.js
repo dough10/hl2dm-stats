3a677c6e29e94a04e79b9dc0a4dc62cf4970717c
dough10 <doughten.jimi@gmail.com>
2021-05-23 16:34:56 -0500

    U: logging, V: 1.1.5

--- api.js
+++ api.js
@@ -184,9 +184,9 @@ function mapStart(logId) {
   if (h < 10) h = `0${h}`;
   let min = now.getMinutes();
   if (min < 10) min = `0${min}`;
+  console.log("appData.playersPlayed", appData.playersPlayed, "appData.demoName", appData.demoName, "fs.existsSync(appData.demoName)", fs.existsSync(appData.demoName));
   if (!appData.playersPlayed && appData.demoName && fs.existsSync(appData.demoName)) {
-    console.log(appData.demoName);
-    // fs.unlinkSync(appData.demoName);
+    fs.unlinkSync(appData.demoName);
   }
   appData.demoName = path.join(config.gameServerDir, `auto-${y}${m}${d}-${h}${min}-dm_bellas_room_d1.dem`);
   appData.playersPlayed = false;
